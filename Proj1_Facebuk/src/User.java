95851f298953d7c2a2af31e362cb6ae399bf8928
Iulian V. Brumar <7262040+iulibrumi@users.noreply.github.com>
2018-10-30 23:53:26 -0400

    Trying to implement largest clique.

--- Proj1_Facebuk/src/User.java
+++ Proj1_Facebuk/src/User.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.awt.Image;
+import java.util.Set
 
 public class User {
     private String _name;
@@ -25,5 +27,38 @@ public class User {
 
     void addFriend (User friend) {
         _friends.add(friend);
+    }
+
+    //friends may not be empty if we execute this method
+    private ArrayList findMaximumCliqueOfFriends(int indexToVisit, ArrayList addedElems) {
+        if (!_friends.containsAll(addedElems)) {
+            return null;
+        } else {
+            ArrayList res1 = findMaximumCliqueOfFriends(indexToVisit + 1, addedElems);
+            addedElems.add(_friends.get(indexToVisit));
+            ArrayList res2 = findMaximumCliqueOfFriends(indexToVisit + 1, addedElems);
+            if (res1.size() > res2.size())
+                return res1;
+            else
+                return res2;
+        }
+    }
+
+    private ArrayList findMaximumCliqueOfFriends(Set visitedFriends) {
+        if (!_friends.containsAll(visitedFriends) || _friends.isEmpty()) {
+            return null;
+        } else {
+            for (int i = 0; i < _friends.size(); ++i) {
+                if (!visitedFriends.contains(_friends.at(i))) {
+                    return null;
+                }
+            }
+        }
+
+    }
+
+    public ArrayList findMaximumCliqueOfFriends () {
+        Set visitedFriends;
+        findMaximumCliqueOfFriends(visitedFriends);
     }
 }
